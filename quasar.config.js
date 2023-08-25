3e2b5935adf3465d0987fd015989733225a67b3f
tamma <koonkoonpond2@gmail.com>
2023-08-25 19:36:22 +0700

    Add 'Notify'in quasar.comf.js

--- quasar.config.js
+++ quasar.config.js
@@ -12,6 +12,11 @@ const { configure } = require('quasar/wrappers')
 
 module.exports = configure(function (/* ctx */) {
   return {
+    framework: {
+      plugins: [
+        'Notify'
+      ],
+    },
     eslint: {
       // fix: true,
       // include: [],
